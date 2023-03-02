a87f988e844325f806a866dcd29651591cf08c22
Ben Wiedermann <benw@cs.hmc.edu>
2023-03-01 21:02:21 -0800

    Add more dcoumentation

--- src/main/scala/picolib/Simulation.scala
+++ src/main/scala/picolib/Simulation.scala
@@ -5,14 +5,19 @@ import picolib.semantics._
 import picolib.display._
 import scalafx.application.JFXApp3
 
-
+/** A main program the can run a list of rules in a maze and display the
+  * simulation details in the terminal
+  */
 class TextSimulation(maze: Maze, rules: List[Rule]) extends App:
-    object bot extends Picobot(maze, rules) with TextDisplay
-    bot.run()
+  object bot extends Picobot(maze, rules) with TextDisplay
+  bot.run()
 
+/** A main program the can run a list of rules in a maze and display the
+  * simulation details in a GUI panel
+  */
 class GUISimulation(maze: Maze, rules: List[Rule]) extends JFXApp3:
 
-  override def start() = { 
+  override def start() = {
     object bot extends Picobot(maze, rules) with TextDisplay with GUIDisplay
-    stage = bot.mainStage 
+    stage = bot.mainStage
   }
